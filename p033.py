f446f95906ea218c11e2d46e3916226568c41cba
OXullo Intersecans <x@brainrapers.org>
2015-01-14 22:17:04 +0100

    Added p033

---
+++ p033.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import utils
+
+nprod = 1
+dprod = 1
+for n in xrange(11, 50):
+	for d in xrange(50, 100):
+		nd = set(utils.num_to_digits(n))
+		dd = set(utils.num_to_digits(d))
+
+		if len(nd) == 1 or len(dd) == 1 or nd == dd or n % 10 == 0 or d % 10 == 0:
+			continue
+
+		common = nd.intersection(dd)
+
+		if common:
+			#print nd, dd, nd - common, dd - common
+			q = float(n) / d
+			nsing = list(nd-common)[0]
+			dsing = list(dd-common)[0]
+
+			if dsing:
+				qsem = float(nsing) / dsing
+				if q == qsem:
+					print nsing, dsing
+					nprod *= nsing
+					dprod *= dsing
+
+print nprod, dprod
---
+++ utils.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def num_to_digits(n):
+	return [int(ns) for ns in str(n)]
