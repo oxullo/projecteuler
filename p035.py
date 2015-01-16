2210a7d57e2b229c419440e67630749bb8dac3e2
OXullo Intersecans <x@brainrapers.org>
2015-01-16 16:17:51 +0100

    Added p035

---
+++ p035.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import calc
+import utils
+
+def all_rotations(nd):
+	results = []
+	for i in xrange(len(nd) - 1):
+		nd.append(nd.pop(0))
+		results.append(nd[:])
+
+	return results
+
+def count_circular(numbers):
+	totals = 0
+
+	while numbers:
+		nd = numbers.pop(0)
+		# Exclude numbers with even and 5 digits
+		if any([(i%2==0 or i%5==0) for i in nd]):
+			continue
+
+		if nd == ([nd[0]] * len(nd)):
+			totals += 1
+		else:
+			flag = True
+			for rot in all_rotations(nd):
+				if rot in numbers:
+					numbers.remove(rot)
+				else:
+					flag = False
+					break
+
+			if flag:
+				totals += len(nd)
+
+	return totals
+
+
+if __name__ == '__main__':
+	try:
+		primes_digits = utils.restore('primes-1M.json')
+		print 'Primes loaded from cache'
+	except:
+		print 'Calculating primes'
+		primes_digits = [p for p in xrange(1, int(10E6)) if calc.isprime(p)]
+		utils.save('primes-1M.json', primes_digits)
+		print 'done'
+
+	totals = len([n for n in primes_digits if n < 10 and n > 1])
+	for i in xrange(2, 7):
+		print 'Processing %d-length digits' % i
+		series = [utils.num_to_digits(n) for n in primes_digits if n >= 10**(i-1) and n < 10**i]
+		totals += count_circular(series)
+
+	print totals
--- utils.py
+++ utils.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+
 def num_to_digits(n):
 	return [int(ns) for ns in str(n)]
 
 def digits_to_num(digits):
 	return int(''.join([str(d) for d in digits]))
+
+def save(fname, data):
+	json.dump(data, open(fname, 'w'))
+
+def restore(fname):
+	return json.load(open(fname))
