9f040f20ec194f63a17a0e11233866bb48634684
OXullo Intersecans <x@brainrapers.org>
2015-01-25 19:48:55 +0100

    A bit more proper version of p050, that at least doesn't fail 1E2 test

--- p050.py
+++ p050.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This is hacky and works just by coincidence
-# in fact, the first test fails, returning a 5-addenda sum
 
+import operator
 import utils
 
 primes = utils.get_primes()
 primes_set = set(primes)
+
 
 def find_max_addenda(top):
     sums = [primes[0]]
 
     for p in primes[1:]:
         last = p + sums[-1]
+        # This is a dangerous assumption
         if last > top:
             break
         sums.append(last)
 
-    for i, s in enumerate(sums):
-        candidate = sums[-1] - s
-        if candidate in primes_set:
-            return len(sums) - i - 1, candidate
+    addenda = []
+    for i in xrange(len(sums) - 1, -1, -1):
+        if sums[i] in primes_set:
+            addenda.append((sums[i], i + 1))
+            break
+        else:
+            for j in xrange(0, i):
+                if (sums[i] - sums[j]) in primes_set:
+                    addenda.append((sums[i] - sums[j], i-j))
+                    break
+
+    addenda.sort(key=operator.itemgetter(1))
+
+    return addenda[-1]
 
 def test():
     print find_max_addenda(100)
     print find_max_addenda(1000)
 
 if __name__ == '__main__':
+    test()
     print find_max_addenda(1E6)
